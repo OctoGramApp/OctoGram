ff3ffd65968a6ad09c0297004a86d4bc75830302
Nick <64551534+null-nick@users.noreply.github.com>
2023-07-07 11:54:07 +0200

    Added missing copyright

--- TMessagesProj/src/main/java/it/octogram/android/preferences/tgkit/preference/OctoPreferences.java
+++ TMessagesProj/src/main/java/it/octogram/android/preferences/tgkit/preference/OctoPreferences.java
@@ -1,3 +1,10 @@
+/*
+ * This is the source code of OctoGram for Android v.2.0.x
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright OctoGram, 2023.
+ */
 package it.octogram.android.preferences.tgkit.preference;
 
 import java.util.ArrayList;
