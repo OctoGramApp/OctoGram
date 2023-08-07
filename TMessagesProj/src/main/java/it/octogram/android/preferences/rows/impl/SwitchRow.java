2dc084e241335448e4be4e792bad7d2959254e85
ImOnlyFire <56583784+ImOnlyFire@users.noreply.github.com>
2023-08-07 21:33:40 +0200

    Added a supplier clickable on `SwitchRow` to execute an action before the switch toggle event

--- TMessagesProj/src/main/java/it/octogram/android/preferences/rows/impl/SwitchRow.java
+++ TMessagesProj/src/main/java/it/octogram/android/preferences/rows/impl/SwitchRow.java
@@ -15,13 +15,17 @@ import it.octogram.android.preferences.rows.BaseRow;
 import it.octogram.android.preferences.rows.Clickable;
 import it.octogram.android.preferences.rows.ToggleableBaseRowBuilder;
 
+import java.util.function.Supplier;
+
 public class SwitchRow extends BaseRow implements Clickable {
 
     private final ConfigProperty<Boolean> preferenceValue;
+    private final Supplier<Boolean> supplierClickable;
 
-    private SwitchRow(@Nullable String title, @Nullable String summary, boolean requiresRestart, ConfigProperty<Boolean> showIf, boolean divider, ConfigProperty<Boolean> preferenceValue) {
+    private SwitchRow(@Nullable String title, @Nullable String summary, boolean requiresRestart, ConfigProperty<Boolean> showIf, boolean divider, ConfigProperty<Boolean> preferenceValue, Supplier<Boolean> supplierClickable) {
         super(title, summary, requiresRestart, showIf, divider, PreferenceType.SWITCH);
         this.preferenceValue = preferenceValue;
+        this.supplierClickable = supplierClickable;
     }
 
     public boolean getPreferenceValue() {
@@ -29,7 +33,10 @@ public class SwitchRow extends BaseRow implements Clickable {
     }
 
     @Override
-    public void onClick(BaseFragment fragment, Activity activity, View view, int position, float x, float y) {
+    public boolean onClick(BaseFragment fragment, Activity activity, View view, int position, float x, float y) {
+        if (supplierClickable != null && !supplierClickable.get()) {
+            return false;
+        }
         OctoConfig.INSTANCE.toggleBooleanSetting(preferenceValue);
 
         TextCheckCell cell = (TextCheckCell) view;
@@ -38,8 +45,15 @@ public class SwitchRow extends BaseRow implements Clickable {
     }
 
     public static class SwitchRowBuilder extends ToggleableBaseRowBuilder<SwitchRow, Boolean> {
+        private Supplier<Boolean> supplierClickable;
+
+        public SwitchRowBuilder onClick(Supplier<Boolean> clickable) {
+            this.supplierClickable = clickable;
+            return this;
+        }
+
         public SwitchRow build() {
-            return new SwitchRow(title, description, requiresRestart, showIf, divider, preferenceValue);
+            return new SwitchRow(title, description, requiresRestart, showIf, divider, preferenceValue, supplierClickable);
         }
     }
 
