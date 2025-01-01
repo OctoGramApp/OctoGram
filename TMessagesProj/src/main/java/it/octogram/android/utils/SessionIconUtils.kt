fbf5e3b020bac7e2a375caffe4f9760a382439c0
nick <64551534+null-nick@users.noreply.github.com>
2025-01-01 09:40:45 +0100

    Update copyright year to 2025.

--- TMessagesProj/src/main/java/it/octogram/android/utils/SessionIconUtils.kt
+++ TMessagesProj/src/main/java/it/octogram/android/utils/SessionIconUtils.kt
@@ -3,7 +3,7 @@
  * It is licensed under GNU GPL v2 or later.
  * You should have received a copy of the license in this archive (see LICENSE).
  *
- * Copyright OctoGram, 2023-2024.
+ * Copyright OctoGram, 2023-2025.
  */
 
 package it.octogram.android.utils
